645fc5b997b3c285ba86973fa6b9b4ccf4cc0953
StevanZivadinovic <stevanzivadinovic@outlook.com>
2024-04-19 21:54:58 +0200

    deploy prepare

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app=express();
 app.use(
   cors({
     // origin: ['http://localhost:3000', 'https://mappin-fe.onrender.com'],
-    origin:'http://localhost:3000',
+    origin:'https://mappin-fe.onrender.com',
     credentials: true,
   })
 );
